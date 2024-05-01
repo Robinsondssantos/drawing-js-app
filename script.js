dc96353510c779387895d8c34e34b5df65d83898
Robinson D. S. Santos <robinsonsantos@inobram.com.br>
2024-05-01 16:52:49 -0300

    tutorial stopped at 35:08

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const canvas = document.querySelector("canvas");
-const toolBtns = document.querySelectorAll(".tool");
+const canvas = document.querySelector("canvas"),
+toolBtns = document.querySelectorAll(".tool"),
+fillColor = document.querySelector("#fill-color");
 
 ctx = canvas.getContext("2d");
 
-let isDrawing = false;
+// Global variables with default value
+let prevMouseX, prevMouseY, snapshot, 
+isDrawing = false,
+selectedTool = "brush",
 brushWidth = 5;
 
 window.addEventListener("load", () => {
@@ -12,21 +16,54 @@ window.addEventListener("load", () => {
     canvas.height = canvas.offsetHeight;
 })
 
-const startDraw = () => {
+const drawRect = (e) => {
+    // if fillColor isn't checked draw a rect with border else draw rect with background
+    if (!fillColor.checked) {
+        // Creating rect according to the mouse pointer
+        return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);        
+    }
+    ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+}
+
+const drawCircle = (e) => {
+    ctx.beginPath(); // Creating new path to draw circle
+    // Getting radius for circle according to the mouse pointer
+    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
+    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
+    fillColor.checked ? ctx.fill() : ctx.stroke(); // if fillColor is checked fill circle else draw border circle
+}
+
+const startDraw = (e) => {
     isDrawing = true;
+    prevMouseX = e.offsetX; // Passing current mouseX position as prevMouseX value
+    prevMouseY = e.offsetY; // Passing current mouseY position as prevMouseY value
     ctx.beginPath(); // Creating new path to draw
     ctx.lineWidth = brushWidth; // Passing brushSize as line width
+    // Copying canvas data & passing as snapshot value.. this avoids dragging the image
+    snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
 const drawing = (e) => {
     if (!isDrawing) return; // if isDrawing is false return from here
-    ctx.lineTo(e.offsetX, e.offsetY); // Creating line according to the mouse pointer
-    ctx.stroke(); // drawing/filling line with color
+    ctx.putImageData(snapshot, 0, 0); // Adding copied canvas data on to this canvas
+
+    if (selectedTool === "brush") {
+        ctx.lineTo(e.offsetX, e.offsetY); // Creating line according to the mouse pointer
+        ctx.stroke(); // drawing/filling line with color
+    } else if (selectedTool === "rectangle") {
+        drawRect(e);
+    } else if (selectedTool === "circle") {
+        drawCircle(e);
+    }
 }
 
 toolBtns.forEach(btn => {
     btn.addEventListener("click", () => { // Adding clicke evnet to all tool option
-        console.log(btn.id);
+        // Removing active class from the previous option and adding on current clicked option
+        document.querySelector(".options .active").classList.remove("active");
+        btn.classList.add("active");
+        selectedTool = btn.id;
+        console.log(selectedTool);
     })
 })
 
